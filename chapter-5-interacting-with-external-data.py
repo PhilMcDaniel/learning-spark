b54d901506059e8f849c5012b0fbf9dc4bee71fe
Phil McDaniel <mcdanipc@gmail.com>
2022-08-12 09:10:54 -0500

    Update chapter-5-interacting-with-external-data.py

--- chapter-5-interacting-with-external-data.py
+++ chapter-5-interacting-with-external-data.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-
+from pyspark.sql.types import LongType
+import pandas as pd
 
 #https://github.com/databricks/learningsparkv2
 spark = (
@@ -10,3 +11,35 @@ spark = (
     .appName("SparkSQLExampleApp")
     .getOrCreate()
 )
+
+#spark SQL UDFs
+def cubed(s):
+    return s*s*s
+
+#register UDF
+spark.udf.register("cubed",cubed,LongType())
+
+spark.range(1,9).createOrReplaceTempView("udf_test")
+spark.sql("SELECT id,cubed(id) FROM udf_test").show()
+
+
+#evaluation order of WHERE clause is not guaranteed. Has implications for NULL checking
+
+#vectorized/Pandas UDF to improve performance (not row by row execution)
+#declare the function
+def cubed_pd(a:pd.Series) -> pd.Series:
+    return a*a*a
+
+#create the udf for the function
+cubed_pd_udf = pandas_udf(cubed_pd,returnType=LongType())
+
+#create series of test data
+x = pd.Series([1,2,3,4,5,6,7,8,9])
+#execute from pandas
+print(cubed_pd(x))
+
+#execute the spark udf
+df=spark.range(1,9)
+df.select("id",cubed_pd_udf(col("id"))).show()
+#checking the spark UI shows a WholeStageCodegen DAG step which indicates better performance
+#ArrowEvalPython DAG step implies pandas UDF usage/execution
